73df4c1749133ed6ca70738e33dd39e46885f00e
zayrats <sugarprasetya@gmail.com>
2024-05-19 17:04:35 +0700

    update lageee

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link'; // Pastikan Link diimpor
-import styles from '../styles/Login.module.css';
+import styles from '../../styles/Login.module.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
