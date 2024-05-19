a2be29e4141af73fadaac7af75a29cf750835aff
zayrats <sugarprasetya@gmail.com>
2024-05-19 18:43:38 +0700

    update lageee3

--- pages/api/register.js
+++ pages/api/register.js
@@ -1,7 +1,7 @@
 import User from '../../models/User';
 import bcrypt from 'bcryptjs';
 // import sequelize from '../../utils/dbConnect';
-import sequelize from '@/utils/dbConnect';
+import sequelize from '@/utils/db';
 
 export default async function handler(req, res) {
   const { email, password, name, phone } = req.body;
